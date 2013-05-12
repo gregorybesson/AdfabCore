d0ed8f9ac9b39606e0e69d279aaac78b65601971
GrG <gregory.besson@adfab.fr>
2013-05-12 14:53:50 +0200

    Ajout de 2 variables : message de partage par défaut et variables adserving par défaut

--- src/AdfabCore/Options/ModuleOptions.php
+++ src/AdfabCore/Options/ModuleOptions.php
@@ -9,15 +9,41 @@ class ModuleOptions extends AbstractOptions
     protected $bitlyUsername = 'o_7t2s2bjmun';
     protected $bitlyApiKey   = 'R_335290ffb3f5fc08b45d3e0e6678c3db';
     protected $bitlyUrl      = 'http://api.bit.ly/v3/shorten';
-
     protected $transport_class = 'Zend\Mail\Transport\File';
     protected $options_class   = 'Zend\Mail\Transport\FileOptions';
     protected $options   = array('path' => 'data/mail/');
     protected $quConfig = array();
     protected $googleAnalytics = array('id' => '');
+    protected $adServing = array();
+    protected $defaultShareMessage = 'Venez jouer';
+    
 
     protected $ckeditor = array();
+    
+    public function getDefaultShareMessage()
+    {
+        return $this->defaultShareMessage;
+    }
+    
+    public function setDefaultShareMessage($defaultShareMessage)
+    {
+        $this->defaultShareMessage = $defaultShareMessage;
+    
+        return $this;
+    }
 
+    public function getAdServing()
+    {
+        return $this->adServing;
+    }
+    
+    public function setAdServing($adServing)
+    {
+        $this->adServing = $adServing;
+    
+        return $this;
+    }
+    
     public function getGoogleAnalytics()
     {
         return $this->googleAnalytics;
